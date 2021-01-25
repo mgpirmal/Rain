c8ec12ca5299a7508b3e45bdfda21ec61954b877
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-01-25 18:13:59 -0500

    Added more html elements

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var instance = M.Sidenav.getInstance(elem);
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
