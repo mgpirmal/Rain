359a72538c1fd1308d749331b70ab877827dde8f
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-01-27 17:31:13 -0500

    started JS and API call function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,62 @@
-var instance = M.Sidenav.getInstance(elem);
+// var search= document.querySelector("#cityInput");
+var search = "Orlando";
+var city1= document.querySelector("#city1");
+var city2= document.querySelector("#city2");
+var city3= document.querySelector("#city3");
+var city4= document.querySelector("#city4");
+var city= document.querySelector("#city");
+var temp= document.querySelector("#temp");
+var humidity= document.querySelector("#humidity");
+var windSpeed= document.querySelector("#windSpeed");
+var uv= document.querySelector("#uv");
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+//added imperial to URL for Fahrenheit
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+// var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=d4498fe291ca2967ed4812e5f7191859";
+
+
+
+
+function call(){
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // After the data comes back from the API
+        .then(function() {
+            
+            });
+
+};
+
+
+
+function start(response){
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // After the data comes back from the API
+        .then(function(response) {
+            console.log(response);
+            console.log(response.main.temp);
+            console.log(response.wind.speed);
+            });
+
+};
+function renderWeather(){
+      
+     temp= response.main.temp
+
+};
+
+
+
+
+
+
+start();
