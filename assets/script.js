b67adae88440e86612308beea3f7352674c8f4c4
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-02-01 20:23:04 -0500

    not working on github

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ var uv= document.querySelector("#uv");
 
 
 //added imperial to URL for Fahrenheit
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+var queryURL = "s://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
 // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=d4498fe291ca2967ed4812e5f7191859";
 
 
@@ -22,7 +22,7 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+search+"&unit
 
 function call(){
   var search = document.getElementById("input").value;
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
 console.log(search);
   $.ajax({
     url: queryURL,
@@ -34,7 +34,7 @@ console.log(search);
 
         // pull icon code from api and add it to url to show on page
         var iconcode = response.weather[0].icon;
-var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
 console.log(iconurl);
         // replace elements eith api response 
         document.getElementById("temp").innerHTML = "Temp: "+response.main.temp+ "F";
@@ -48,7 +48,7 @@ console.log(iconurl);
 
 // Below is code for forecast 
 // ==================================================================================
-var query2URL = "http://api.openweathermap.org/data/2.5/forecast?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+var query2URL = "https://api.openweathermap.org/data/2.5/forecast?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
 $.ajax({
   url: query2URL,
   method: "GET"
@@ -57,35 +57,35 @@ $.ajax({
   .then(function(response) {
       console.log(response);
       var iconcode1 = response.list[0].weather[0].icon;
-var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+var iconurl1 = "https://openweathermap.org/img/w/" + iconcode1 + ".png";
 console.log(iconurl1);
 document.getElementById("date1").innerHTML = response.list[0].dt_txt;
       document.getElementById("temp1").innerHTML = "Temp: "+response.list[0].main.temp_max+ "F / "+response.list[0].main.temp_min+ "F";
       $('#wicon1').attr('src', iconurl1);
 // Temp every 24 hours (each list is 3 hours)
       var iconcode2 = response.list[8].weather[0].icon;
-      var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+      var iconurl2 = "https://openweathermap.org/img/w/" + iconcode2 + ".png";
       console.log(iconurl2);
       document.getElementById("date2").innerHTML = response.list[8].dt_txt;
             document.getElementById("temp2").innerHTML = "Temp: "+response.list[8].main.temp_max+ "F / "+response.list[8].main.temp_min+ "F";
             $('#wicon2').attr('src', iconurl2);
 
             var iconcode3 = response.list[16].weather[0].icon;
-            var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+            var iconurl3 = "https://openweathermap.org/img/w/" + iconcode3 + ".png";
             console.log(iconurl3);
             document.getElementById("date3").innerHTML = response.list[16].dt_txt;
                   document.getElementById("temp3").innerHTML = "Temp: "+response.list[16].main.temp_max+ "F / "+response.list[16].main.temp_min+ "F";
                   $('#wicon3').attr('src', iconurl3);
 
                   var iconcode4 = response.list[24].weather[0].icon;
-                  var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+                  var iconurl4 = "https://openweathermap.org/img/w/" + iconcode4 + ".png";
                   console.log(iconurl4);
                   document.getElementById("date4").innerHTML = response.list[24].dt_txt;
                         document.getElementById("temp4").innerHTML = "Temp: "+response.list[24].main.temp_max+ "F / "+response.list[24].main.temp_min+ "F";
                         $('#wicon4').attr('src', iconurl4);
 
                         var iconcode5 = response.list[32].weather[0].icon;
-                        var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
+                        var iconurl5 = "https://openweathermap.org/img/w/" + iconcode5 + ".png";
                         console.log(iconurl5);
                         document.getElementById("date5").innerHTML = response.list[32].dt_txt;
                               document.getElementById("temp5").innerHTML = "Temp: "+response.list[32].main.temp_max+ "F / "+response.list[32].main.temp_min+ "F";
@@ -101,7 +101,7 @@ document.getElementById("date1").innerHTML = response.list[0].dt_txt;
 
 function start(response){
   var search = "orlando"
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
 
     $.ajax({
         url: queryURL,
@@ -111,7 +111,7 @@ function start(response){
         .then(function(response) {
             console.log(response);
             var iconcode = response.weather[0].icon;
-var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
 console.log(iconurl);
             document.getElementById("temp").innerHTML = "Temp: "+response.main.temp+ "F";
             document.getElementById("windSpeed").innerHTML = "Wind Speed: "+response.wind.speed+ "MPH";
@@ -123,7 +123,7 @@ console.log(iconurl);
             });
             // Below is code for forecast 
 // ==================================================================================
-var query2URL = "http://api.openweathermap.org/data/2.5/forecast?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
+var query2URL = "https://api.openweathermap.org/data/2.5/forecast?q="+search+"&units=imperial&appid=d4498fe291ca2967ed4812e5f7191859";
 $.ajax({
   url: query2URL,
   method: "GET"
@@ -132,35 +132,35 @@ $.ajax({
   .then(function(response) {
       console.log(response);
       var iconcode1 = response.list[0].weather[0].icon;
-var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+var iconurl1 = "https://openweathermap.org/img/w/" + iconcode1 + ".png";
 console.log(iconurl1);
       document.getElementById("date1").innerHTML = response.list[0].dt_txt;
       document.getElementById("temp1").innerHTML = "Temp: "+response.list[0].main.temp_max+ "F / "+response.list[0].main.temp_min+ "F";
       $('#wicon1').attr('src', iconurl1);
 // Temp every 24 hours (each list is 3 hours)
       var iconcode2 = response.list[8].weather[0].icon;
-      var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+      var iconurl2 = "https://openweathermap.org/img/w/" + iconcode2 + ".png";
       console.log(iconurl2);
       document.getElementById("date2").innerHTML = response.list[8].dt_txt;
             document.getElementById("temp2").innerHTML = "Temp: "+response.list[8].main.temp_max+ "F / "+response.list[8].main.temp_min+ "F";
             $('#wicon2').attr('src', iconurl2);
 
             var iconcode3 = response.list[16].weather[0].icon;
-            var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+            var iconurl3 = "https://openweathermap.org/img/w/" + iconcode3 + ".png";
             console.log(iconurl3);
             document.getElementById("date3").innerHTML = response.list[16].dt_txt;
                   document.getElementById("temp3").innerHTML = "Temp: "+response.list[16].main.temp_max+ "F / "+response.list[16].main.temp_min+ "F";
                   $('#wicon3').attr('src', iconurl3);
 
                   var iconcode4 = response.list[24].weather[0].icon;
-                  var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+                  var iconurl4 = "https://openweathermap.org/img/w/" + iconcode4 + ".png";
                   console.log(iconurl4);
                   document.getElementById("date4").innerHTML = response.list[24].dt_txt;
                         document.getElementById("temp4").innerHTML = "Temp: "+response.list[24].main.temp_max+ "F / "+response.list[24].main.temp_min+ "F";
                         $('#wicon4').attr('src', iconurl4);
 
                         var iconcode5 = response.list[32].weather[0].icon;
-                  var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
+                  var iconurl5 = "https://openweathermap.org/img/w/" + iconcode5 + ".png";
                   console.log(iconurl5);
                   document.getElementById("date5").innerHTML = response.list[32].dt_txt;
                         document.getElementById("temp5").innerHTML = "Temp: "+response.list[32].main.temp_max+ "F / "+response.list[32].main.temp_min+ "F";
